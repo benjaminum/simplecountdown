2becf9ed53242b8a510df98db7664b4d7a7440bd
Benjamin Ummenhofer <benjaminum@gmail.com>
2010-03-28 12:18:24 +0200

    text size changes when the widget is resized

--- contents/code/main.py
+++ contents/code/main.py
@@ -41,36 +41,53 @@ class SimpleCountdown(plasmascript.Applet):
         self.theme = Plasma.Svg(self)
         self.setBackgroundHints(Plasma.Applet.DefaultBackground)
 
+        # Try long strings first
+        self.shortStrings = False
+
         # Add labels to the layout
         self.layout = QGraphicsLinearLayout(Qt.Vertical, self.applet)
         self.eventLabel = Plasma.Label(self.applet)
-        self.eventLabel.setAlignment(Qt.AlignHCenter)
+        self.eventLabel.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
         self.eventLabel.setText('Event');
-        self.eventLabel.setStyleSheet('font-size:18px')
         self.timeLabel = Plasma.Label(self.applet)
-        self.timeLabel.setAlignment(Qt.AlignHCenter)
+        self.timeLabel.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
         self.timeLabel.setText('Time remaining')
-        self.timeLabel.setStyleSheet('font-size:24px; font-weight:bold')
+        
 
         self.layout.addItem(self.eventLabel)
         self.layout.addItem(self.timeLabel)
         self.applet.setLayout(self.layout)
-        self.resize(375, 85)
+
+        self.setMinimumSize(150,60)
+        #self.resize(375, 100)
+
 
         # Setup timer
         self.timer = QTimer()
         self.timer.timeout.connect(self.updateLabel)
 
         self.readConfig()
+
+        self.computeFontSizeTable()
+        self.setLabelFonts()
+        
         self.startCountDown( self.dateTime )
 
 
+    def sizeHint(self, which, constraint):
+        if which == Qt.PreferredSize:
+           return QSizeF(375,100)
+        else:
+           return plasmascript.Applet.sizeHint(self, which, contraint)
+       
 
 
     def paintInterface(self, painter, option, rect):
         painter.save()
-        #painter.setPen(Qt.white)
-        #painter.drawText(rect, Qt.AlignVCenter | Qt.AlignHCenter, "Hello Pyt    hon!")
+        painter.setPen(Qt.white)
+        #painter.drawText(self.eventLabel.geometry(), Qt.AlignVCenter | Qt.AlignHCenter, "Hello Python!")
+        #painter.drawRect(self.eventLabel.geometry())
+        #painter.drawRect(self.timeLabel.geometry())
         painter.restore()
 
 
@@ -83,11 +100,68 @@ class SimpleCountdown(plasmascript.Applet):
            self.saveConfig()
         
 
+    def constraintsEvent(self, constraints):
+        self.setLabelFonts()
+        if int(constraints) == Plasma.SizeConstraint:
+           cfg = self.config()
+           cfg.writeEntry('geometry', self.geometry() )
+
+        plasmascript.Applet.constraintsEvent(self, constraints)
+
+
+    def computeFontSizeTable(self):
+        self.fontSizeTable = []
+        longestString = '   99 hours 99 minutes   '
+        
+        for pixSize in range(100,11,-1):
+            font = QFont()
+            font.setPixelSize(pixSize)
+            font.setBold(True)
+            fontMetric = QFontMetrics(font)
+            w = fontMetric.width( longestString )
+            h = fontMetric.height()
+            self.fontSizeTable.append( (w, h, pixSize) )
+        #print self.fontSizeTable
+
+
+
+    def setLabelFonts(self):
+        i = 0
+        width   = self.fontSizeTable[i][0]
+        height  = self.fontSizeTable[i][1]
+        pixSize = self.fontSizeTable[i][2]
+        minPixSize = self.fontSizeTable[len(self.fontSizeTable)-1][2]
+
+        timeLabelWidth  = self.geometry().width() - self.getContentsMargins()[0] - self.getContentsMargins()[2]
+        timeLabelHeight = self.geometry().height() // 2 - self.getContentsMargins()[1] - self.getContentsMargins()[3]
+
+        while (width > timeLabelWidth or height > timeLabelHeight) and i < len(self.fontSizeTable):
+              width   = self.fontSizeTable[i][0]
+              height  = self.fontSizeTable[i][1]
+              pixSize = self.fontSizeTable[i][2]
+              i = i + 1
+
+        if i == len(self.fontSizeTable) and width > timeLabelWidth:
+           self.shortStrings = True
+        else:
+           self.shortStrings = False
+        self.updateLabel()
+
+        self.eventLabel.setStyleSheet('font-size:'+str(pixSize-4)+'px')
+        self.timeLabel.setStyleSheet('font-size:'+str(pixSize)+'px; font-weight:bold')
+
+
 
     def readConfig(self):
         cfg = self.config()
         self.eventLabel.setText( cfg.readEntry('event',QString('Event')).toString() )
         self.dateTime = cfg.readEntry('dateTime', QDateTime() ).toDateTime()
+
+        # Setup default geometry
+        defaultGeometry = self.geometry()
+        defaultGeometry.setSize( self.sizeHint(Qt.PreferredSize, QSizeF()) )
+        # load stored geometry
+        self.setGeometry( cfg.readEntry('geometry', defaultGeometry ).toRectF() )
 
 
            
@@ -95,6 +169,7 @@ class SimpleCountdown(plasmascript.Applet):
         cfg = self.config()
         cfg.writeEntry('event',self.eventLabel.text())
         cfg.writeEntry('dateTime', self.dateTime )
+        cfg.writeEntry('geometry', self.geometry() )
         cfg.sync()
 
 
@@ -140,34 +215,60 @@ class SimpleCountdown(plasmascript.Applet):
            minutes = (seconds // 60) % 60
            seconds = seconds % 60
 
+           if self.shortStrings == True:
+              days_str_pl = ' d '
+              days_str_sg = ' d '
+              hour_str_pl = ' h '
+              hour_str_sg = ' h '
+              min_str_pl  = ' m '
+              min_str_sg  = ' m '
+              sec_str_pl  = ' s '
+              sec_str_sg  = ' s '
+           else:
+              days_str_pl = ' days '
+              days_str_sg = ' day '
+              hour_str_pl = ' hours '
+              hour_str_sg = ' hour '
+              min_str_pl  = ' minutes '
+              min_str_sg  = ' minute '
+              sec_str_pl  = ' secs '
+              sec_str_sg  = ' sec '
+
+
            time_string = ' '
            if days > 1:
-              time_string += str(days) + ' days '
+              time_string += str(days) + days_str_pl
            elif days > 0:
-              time_string += str(days) + ' day '
+              time_string += str(days) + days_str_sg
 
            if days > 0 or hours > 0:
               if hours > 1 or hours == 0:
-                 time_string += str(hours) + ' hours '
+                 time_string += str(hours) + hour_str_pl
               else:
-                 time_string += str(hours) + ' hour '
+                 time_string += str(hours) + hour_str_sg
 
            if days == 0:
               if minutes > 1:
-                 time_string += str(minutes) + ' minutes '
+                 time_string += str(minutes) + min_str_pl
               elif minutes == 1 or (minutes == 0 and hours > 0):
-                 time_string += str(minutes) + ' minute '
+                 time_string += str(minutes) + min_str_sg
               
            if days == 0 and hours == 0:
               if seconds > 1 or seconds == 0:
-                 time_string += str(seconds) + ' secs '
+                 time_string += str(seconds) + sec_str_pl
               else:
-                 time_string += str(seconds) + ' sec '
+                 time_string += str(seconds) + sec_str_sg
+
 
            self.timeLabel.setText(time_string)     
+        else:
+           self.update()
+
 
         
 
  
 def CreateApplet(parent):
     return SimpleCountdown(parent)
+
+
